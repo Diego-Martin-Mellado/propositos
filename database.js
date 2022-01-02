3939849004095cc2ddbbdc6343e37c86624db8d4
Apuntes-Diego <89017608+Diego-Martin-Mellado@users.noreply.github.com>
2022-01-02 01:39:32 +0100

    Cambio la estructura de ficheros y creo un fichero con el json dividido en años

---
+++ database.js
@@ -0,0 +1,88 @@
+var database = {
+		'2022': [
+		// {
+		// 	title: '',
+		// 	description: '', 
+		// 	start: new Date (),
+		// 	end: new Date ()
+		// },
+		{
+			title: 'Sacarse el -Postgrado de Inteligencia Artificial y Machine Learning-',
+			description: '<u>Ponte las pilas y echale huevos</u>, que está difícil, pero no imposible.<br><br>Sólo se trata de llevar todo al día, y siéntete satisfecho si lo has intentado, ten en cuenta que has de apretar según se vaya acercando septiembre, por eso hay menos propósitos para entonces.<br><br><span class="text-danger">¡Muchos ni siquiera lo intentan!</span>',
+			start: new Date ('2022-01-01'),
+			end: new Date ('2022-09-30')
+		},
+		{
+			title: 'Leer un libro cada 2 meses',
+			description: 'Puedes aumentar la velocidad, de hecho es preferible que lo hagas. Podrías empezar con \'Padre rico, padre pobre\', luego deberías seguir con \'Inteligencia emocional\', luego planteate seguir con \'Secretos de la mente millonaria\', \'El código del dinero\', \'Piense y hágase rico\', \'El millonario de la puerta de al lado\' o \'El hombre más rico de Babilonia\'.', 
+			start: new Date ('2022-01-01'),
+			end: new Date ('2022-12-31')
+		},
+		{
+			title: 'Ir al gimnasio',
+			description: 'Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.',
+			start: new Date ('2022-01-03'),
+			end: new Date ('2022-07-01')
+		},
+		{
+			title: 'Sacarse el teorico de conducir',
+			description: 'Concentrate a muerte y sacate el carnet éste mes, sólo este mes, apuntate la última semana y estudia todo, quitatelo de encima.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>', 
+			start: new Date ('2022-07-01'),
+			end: new Date ('2022-07-31')
+		},
+		{
+			title: 'Sacarse el práctico de conducir',
+			description: 'Éste es más fácil, ya sabes manejar un coche mas o menos, has llevado el Mercedes, no de dejes calentar por la cantidad de veces que te lo digan.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>', 
+			start: new Date ('2022-08-01'),
+			end: new Date ('2022-09-30')
+		},
+		{
+			title: 'Crear cursos de Udemy',
+			description: 'Planifica el mes, saca 2 cursos de Udemy, el primero será el de \'Asistente Virtual en Python sin Inteligencia Artificial\', el segundo, \'CRUD básico con PHP y MySQL (MVC)\'.<br><br>La clave es organizarlo bien, con concentración sacas el del Asistente asequiblemente, el CRUD está tirado para tí, <u>organiza</u>.', 
+			start: new Date ('2022-10-01'),
+			end: new Date ('2022-10-31')
+		},
+		{
+			title: 'Volver al gimnasio',
+			description: 'Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.',
+			start: new Date ('2022-11-01'),
+			end: new Date ('2023-06-30')
+		},
+		{
+			title: 'Leer \'Padre rico, padre pobre\'',
+			description: '',
+			start: new Date ('2022-01-01'),
+			end: new Date ('2022-02-28')
+		},
+		{
+			title: 'Leer \'Inteligencia emocional\'',
+			description: '',
+			start: new Date ('2022-03-01'),
+			end: new Date ('2022-04-30')
+		},
+		{
+			title: 'Leer \'El hombre más rico de Babilonia\'',
+			description: '',
+			start: new Date ('2022-05-01'),
+			end: new Date ('2022-06-30')
+		},
+		{
+			title: 'Leer \'Secretos de una mente millonaria\'',
+			description: '',
+			start: new Date ('2022-07-01'),
+			end: new Date ('2022-08-31')
+		},
+		{
+			title: 'Leer \'El millonario de la puerta de al lado\'',
+			description: '',
+			start: new Date ('2022-09-01'),
+			end: new Date ('2022-10-31')
+		},
+		{
+			title: 'Leer \'Piense y hágase rico\'',
+			description: '',
+			start: new Date ('2022-11-01'),
+			end: new Date ('2022-12-31')
+		}
+	]
+}
