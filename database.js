16151f680a595786357d4f93ede366afbe4f63dc
Diego-Martin-Mellado <89017608+Diego-Martin-Mellado@users.noreply.github.com>
2024-09-11 12:07:44 +0200

    Actualizacion 2025

--- database.js
+++ database.js
@@ -1,66 +1,66 @@
 var database = {
-		'2022': [
+	2022: [
 		// {
 		// 	title: '',
-		// 	description: '', 
+		// 	description: '',
 		// 	start: new Date (),
 		// 	end: new Date ()
 		// },
 		{
-			title: 'Sacarse el -Postgrado de Inteligencia Artificial y Machine Learning-',
+			title: "Sacarse el -Postgrado de Inteligencia Artificial y Machine Learning-",
 			description: '<u>Ponte las pilas y echale huevos</u>, que está difícil, pero no imposible.<br><br>Sólo se trata de llevar todo al día, y siéntete satisfecho si lo has intentado, ten en cuenta que has de apretar según se vaya acercando septiembre, por eso hay menos propósitos para entonces.<br><br><span class="text-danger">¡Muchos ni siquiera lo intentan!</span>',
-			start: new Date ('2022-01-01'),
-			end: new Date ('2022-09-30'),
-			abandoned: true
+			start: new Date("2022-01-01"),
+			end: new Date("2022-09-30"),
+			abandoned: true,
 		},
 		{
-			title: 'Leer un libro cada 2 meses',
-			description: 'Puedes aumentar la velocidad, de hecho es preferible que lo hagas. Podrías empezar con \'Padre rico, padre pobre\', luego deberías seguir con \'Inteligencia emocional\', luego planteate seguir con \'Secretos de la mente millonaria\', \'El código del dinero\', \'Piense y hágase rico\', \'El millonario de la puerta de al lado\' o \'El hombre más rico de Babilonia\'.', 
-			start: new Date ('2022-01-01'),
-			end: new Date ('2022-12-31')
+			title: "Leer un libro cada 2 meses",
+			description: "Puedes aumentar la velocidad, de hecho es preferible que lo hagas. Podrías empezar con 'Padre rico, padre pobre', luego deberías seguir con 'Inteligencia emocional', luego planteate seguir con 'Secretos de la mente millonaria', 'El código del dinero', 'Piense y hágase rico', 'El millonario de la puerta de al lado' o 'El hombre más rico de Babilonia'.",
+			start: new Date("2022-01-01"),
+			end: new Date("2022-12-31"),
 		},
 		{
-			title: 'Ir al gimnasio',
-			description: 'Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.',
-			start: new Date ('2022-01-03'),
-			end: new Date ('2022-07-01')
+			title: "Ir al gimnasio",
+			description: "Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.",
+			start: new Date("2022-01-03"),
+			end: new Date("2022-07-01"),
 		},
 		{
-			title: 'Sacarse el teorico de conducir',
-			description: 'Concentrate a muerte y sacate el carnet éste mes, sólo este mes, apuntate la última semana y estudia todo, quitatelo de encima.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>', 
-			start: new Date ('2022-07-01'),
-			end: new Date ('2022-07-31')
+			title: "Sacarse el teorico de conducir",
+			description: 'Concentrate a muerte y sacate el carnet éste mes, sólo este mes, apuntate la última semana y estudia todo, quitatelo de encima.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>',
+			start: new Date("2022-07-01"),
+			end: new Date("2022-07-31"),
 		},
 		{
-			title: 'Sacarse el práctico de conducir',
-			description: 'Éste es más fácil, ya sabes manejar un coche mas o menos, has llevado el Mercedes, no de dejes calentar por la cantidad de veces que te lo digan.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>', 
-			start: new Date ('2022-08-01'),
-			end: new Date ('2022-09-30')
+			title: "Sacarse el práctico de conducir",
+			description: 'Éste es más fácil, ya sabes manejar un coche mas o menos, has llevado el Mercedes, no de dejes calentar por la cantidad de veces que te lo digan.<br><br><span class="text-danger">Sabes que te toca las narices que te lo digan cada 2 &times; 3</span>',
+			start: new Date("2022-08-01"),
+			end: new Date("2022-09-30"),
 		},
 		{
-			title: 'Crear cursos de Udemy',
-			description: 'Planifica el mes, saca 2 cursos de Udemy, el primero será el de \'Asistente Virtual en Python sin Inteligencia Artificial\', el segundo, \'CRUD básico con PHP y MySQL (MVC)\'.<br><br>La clave es organizarlo bien, con concentración sacas el del Asistente asequiblemente, el CRUD está tirado para tí, <u>organiza</u>.', 
-			start: new Date ('2022-10-01'),
-			end: new Date ('2022-10-31')
+			title: "Crear cursos de Udemy",
+			description: "Planifica el mes, saca 2 cursos de Udemy, el primero será el de 'Asistente Virtual en Python sin Inteligencia Artificial', el segundo, 'CRUD básico con PHP y MySQL (MVC)'.<br><br>La clave es organizarlo bien, con concentración sacas el del Asistente asequiblemente, el CRUD está tirado para tí, <u>organiza</u>.",
+			start: new Date("2022-10-01"),
+			end: new Date("2022-10-31"),
 		},
 		{
-			title: 'Volver al gimnasio',
-			description: 'Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.',
-			start: new Date ('2022-11-01'),
-			end: new Date ('2023-06-30')
+			title: "Volver al gimnasio",
+			description: "Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.",
+			start: new Date("2022-11-01"),
+			end: new Date("2023-06-30"),
 		},
 		{
-			title: 'Leer \'Padre rico, padre pobre\'',
-			description: '',
-			start: new Date ('2022-01-01'),
-			end: new Date ('2022-02-28')
+			title: "Leer 'Padre rico, padre pobre'",
+			description: "",
+			start: new Date("2022-01-01"),
+			end: new Date("2022-02-28"),
 		},
 		{
-			title: 'Leer \'El cuadrante del flujo del dinero\'',
-			description: '',
-			start: new Date ('2022-03-01'),
-			end: new Date ('2022-04-30'),
-			completed: true
+			title: "Leer 'El cuadrante del flujo del dinero'",
+			description: "",
+			start: new Date("2022-03-01"),
+			end: new Date("2022-04-30"),
+			completed: true,
 		},
 		// {
 		// 	title: 'Leer \'Inteligencia emocional\'',
@@ -75,16 +75,16 @@ var database = {
 		// 	end: new Date ('2022-06-30')
 		// },
 		{
-			title: 'Leer \'Guía para invertir\'',
-			description: '',
-			start: new Date ('2022-05-01'),
-			end: new Date ('2022-06-30')
+			title: "Leer 'Guía para invertir'",
+			description: "",
+			start: new Date("2022-05-01"),
+			end: new Date("2022-06-30"),
 		},
 		{
-			title: 'Leer \'Inteligencia emocional\'',
-			description: '',
-			start: new Date ('2022-07-01'),
-			end: new Date ('2022-08-31')
+			title: "Leer 'Inteligencia emocional'",
+			description: "",
+			start: new Date("2022-07-01"),
+			end: new Date("2022-08-31"),
 		},
 		// {
 		// 	title: 'Leer \'Secretos de una mente millonaria\'',
@@ -93,24 +93,51 @@ var database = {
 		// 	end: new Date ('2022-08-31')
 		// },
 		{
-			title: 'Leer \'El millonario de la puerta de al lado\'',
-			description: '',
-			start: new Date ('2022-09-01'),
-			end: new Date ('2022-10-31')
+			title: "Leer 'El millonario de la puerta de al lado'",
+			description: "",
+			start: new Date("2022-09-01"),
+			end: new Date("2022-10-31"),
 		},
 		{
-			title: 'Leer \'Piense y hágase rico\'',
-			description: '',
-			start: new Date ('2022-11-01'),
-			end: new Date ('2022-12-31')
-		}
-	], 
-	'2023': [
+			title: "Leer 'Piense y hágase rico'",
+			description: "",
+			start: new Date("2022-11-01"),
+			end: new Date("2022-12-31"),
+		},
+	],
+	2023: [
 		{
-			title: 'Ir al gimnasio',
-			description: 'Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.',
-			start: new Date ('2023-01-03'),
-			end: new Date ('2023-07-01')
-		}
-	]
+			title: "Ir al gimnasio",
+			description: "Tengo que ir al gimnasio. Todos los días excepto 2 a la semana, preferiblemente miercoles, viernes o domingo, dos de ellos.",
+			start: new Date("2023-01-03"),
+			end: new Date("2023-07-01"),
+		},
+	],
+	2024: [],
+	2025: [
+		{
+			title: "Objetivos en el gimnaio",
+			description: "Subir de peso corporal y mantenerlo entre 71kg y 74kg. En cuanto a ejercicios: 100kg en press banca y 200kg en hip thrust.",
+			start: new Date("2024-09-01"),
+			end: new Date("2025-03-21"),
+		},
+		{
+			title: "Ir al gimnasio",
+			description: "Se trata principalmente de constancia, 3-4 días y junto con comer bien y descansar, ya verás que todo irá mejor. Intenta estar allí todos los días a las 21:00 ya cenado.",
+			start: new Date("2024-09-01"),
+			end: new Date("2025-07-01"),
+		},
+		{
+			title: "Programar con constancia en las aplicaciones de 73 apps",
+			description: "No hay un objetivo, pero piensalo como que tienes que sacar algo, siempre hay cosas interesantes que se pueden sacar, como un calendario, un chat, un gestor de archivos, un 'habit tracker', una aplicación de entrenamiento y probablemente muchas cosas más. Sólo tienes que seguir poco a poco. Otra idea chula podría ser aprender a hacer aplicaciones moviles y hacerlas también.",
+			start: new Date("2024-09-01"),
+			end: new Date("2025-07-01"),
+		},
+		{
+			title: "Leer el Diario Estoico y el Diario para Padres Estoicos",
+			description: "Éste hábito también trata únicamente de la constancia",
+			start: new Date("2024-09-01"),
+			end: new Date("2025-12-31"),
+		},
+	],
 }
